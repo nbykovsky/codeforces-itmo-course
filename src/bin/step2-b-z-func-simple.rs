dabf2939907a28669e7248dcd31d8844b3b1c2fc
Nikolay Bykovsky <nikolay.bykovsky@gmail.com>
2021-05-18 20:44:33 +1000

    added simple z func

---
+++ src/bin/step2-b-z-func-simple.rs
@@ -0,0 +1,17 @@
+use std::io;
+
+
+fn main(){
+    let mut line = String::new();
+    io::stdin().read_line(&mut line).expect("unable to read");
+    let line = line.trim().chars().collect::<Vec<char>>();
+    let n:usize = line.len();
+    let mut z:Vec<usize> = vec![0;n];
+    for i in 1..n {
+        while i + z[i] < n && line[z[i]] == line[z[i]+i] {
+            z[i] += 1;
+        }
+    }
+    println!("{}", z.iter().map(|x| x.to_string()).collect::<Vec<String>>().join(" "))
+
+}
